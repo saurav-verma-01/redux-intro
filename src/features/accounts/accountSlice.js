fea65e87cc1b7c42c9cc1e864ecb06d4ef31b460
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-26 15:09:29 +0530

    redux toolkit

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,9 +1,36 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance = state.balance + action.payload;
+    },
+    withdraw(state, action) {
+      state.balance = state.balance - action.payload;
+    },
+    requestLoan(state, action) {
+      if (state.load > 0) return;
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+    },
+    payLoan(state) {
+      state.loan = 0;
+      state.loanPurpose = "";
+      state.balance -= state.loan;
+    },
+  },
+});
+
+/*
 const accountReducer = (state = initialStateAccount, action) => {
   switch (action.type) {
     case "account/deposit":
@@ -36,8 +63,22 @@ const accountReducer = (state = initialStateAccount, action) => {
 
 // Account Dispatchers
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    const host = "api.frankfurter.app";
+    // API Calls
+    const res = await fetch(
+      `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    console.log(data);
+    const converted = data.rates.USD;
+    // Return Action
+
+    return { type: "account/deposit", payload: converted };
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
@@ -62,3 +103,6 @@ export function payLoan() {
 // console.log(store.getState());
 
 export default accountReducer;
+
+
+*/
